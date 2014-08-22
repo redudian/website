15ac7abd7c18ccc0af6d6ee345f73fddd0958eb5
jifei <jifei@outlook.com>
2014-08-23 01:09:43 +0800

    style

---
+++ app/views/index.blade.php
@@ -0,0 +1,325 @@
+@include('includes.head')
+@include('includes.header')
+<div id="content">
+    <div id="content-container">
+        <div id="timeline">
+
+            <div class="feed-item">
+                <div class="vote">
+                    <a class="vote-up"></a>
+
+                    <div class="vote-count">10</div>
+                    <a class="vote-down"></a>
+                </div>
+
+                <div class="feed">
+                    <div class="feed-content"><a href="http://toutiao.com/group/3460056296/">习近平宣布南京青奥会开幕 会见多名外国政要</a>
+                    </div>
+                    <div class="feed-extend">
+
+                        <div class="feed-from"><span>今天&nbsp;10:10</span>&nbsp;&nbsp;<a href="">jifei</a>&nbsp;通过手机发布至&nbsp;<a
+                                href="">新闻</a></div>
+
+                        <div class="feed-handle"><a>评论</a><span class="separator">|</span><a>分享</a><span
+                                class="separator">|</span><a>收藏</a><span class="separator">|</span>举报&nbsp;
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+
+            <div class="feed-item">
+                <div class="vote">
+                    <a class="vote-up"></a>
+
+                    <div class="vote-count">10</div>
+                    <a class="vote-down"></a>
+                </div>
+
+                <div class="feed">
+                    <div class="feed-content"><a href="http://toutiao.com/group/3460056296/">抢先实拍全新科鲁兹1.4T</a></div>
+                    <div class="feed-extend">
+
+                        <div class="feed-from"><span>今天&nbsp;10:10</span>&nbsp;&nbsp;<a href="">jifei</a>&nbsp;通过手机发布至&nbsp;<a
+                                href="">新闻</a></div>
+
+                        <div class="feed-handle"><a>评论</a><span class="separator">|</span><a>分享</a><span
+                                class="separator">|</span><a>收藏</a><span class="separator">|</span>举报&nbsp;
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+
+            <div class="feed-item">
+                <div class="vote">
+                    <a class="vote-up"></a>
+
+                    <div class="vote-count">10</div>
+                    <a class="vote-down"></a>
+                </div>
+
+                <div class="feed">
+                    <div class="feed-content"><a href="http://toutiao.com/group/3460056296/">作为程序员你写过的最漂亮的代码是什么</a>
+                    </div>
+                    <div class="feed-extend">
+
+                        <div class="feed-from"><span>今天&nbsp;10:10</span>&nbsp;&nbsp;<a href="">jifei</a>&nbsp;通过手机发布至&nbsp;<a
+                                href="">新闻</a></div>
+
+                        <div class="feed-handle"><a>评论</a><span class="separator">|</span><a>分享</a><span
+                                class="separator">|</span><a>收藏</a><span class="separator">|</span>举报&nbsp;
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+
+            <div class="feed-item">
+                <div class="vote">
+                    <a class="vote-up"></a>
+
+                    <div class="vote-count">10</div>
+                    <a class="vote-down"></a>
+                </div>
+
+                <div class="feed">
+                    <div class="feed-content"><a href="http://toutiao.com/group/3460056296/">编程开发中最浪费时间和资源的7个错误</a>
+                    </div>
+                    <div class="feed-extend">
+
+                        <div class="feed-from"><span>今天&nbsp;10:10</span>&nbsp;&nbsp;<a href="">jifei</a>&nbsp;通过手机发布至&nbsp;<a
+                                href="">新闻</a></div>
+
+                        <div class="feed-handle"><a>评论</a><span class="separator">|</span><a>分享</a><span
+                                class="separator">|</span><a>收藏</a><span class="separator">|</span>举报&nbsp;
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+            <div class="feed-item">
+                <div class="vote">
+                    <a class="vote-up"></a>
+
+                    <div class="vote-count">10</div>
+                    <a class="vote-down"></a>
+                </div>
+
+                <div class="feed">
+                    <div class="feed-content"><a href="http://toutiao.com/group/3460056296/">抢先实拍全新科鲁兹1.4T</a></div>
+                    <div class="feed-extend">
+
+                        <div class="feed-from"><span>今天&nbsp;10:10</span>&nbsp;&nbsp;<a href="">jifei</a>&nbsp;通过手机发布至&nbsp;<a
+                                href="">新闻</a></div>
+
+                        <div class="feed-handle"><a>评论</a><span class="separator">|</span><a>分享</a><span
+                                class="separator">|</span><a>收藏</a><span class="separator">|</span>举报&nbsp;
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+            <div class="feed-item">
+                <div class="vote">
+                    <a class="vote-up"></a>
+
+                    <div class="vote-count">10</div>
+                    <a class="vote-down"></a>
+                </div>
+
+                <div class="feed">
+                    <div class="feed-content"><a href="http://toutiao.com/group/3460056296/">抢先实拍全新科鲁兹1.4T</a></div>
+                    <div class="feed-extend">
+
+                        <div class="feed-from"><span>今天&nbsp;10:10</span>&nbsp;&nbsp;<a href="">jifei</a>&nbsp;通过手机发布至&nbsp;<a
+                                href="">新闻</a></div>
+
+                        <div class="feed-handle"><a>评论</a><span class="separator">|</span><a>分享</a><span
+                                class="separator">|</span><a>收藏</a><span class="separator">|</span>举报&nbsp;
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+        </div>
+        <div id="dashboard-right">
+            <div id="tagscloud">
+                <a href="http://www.17sucai.com/" class="tagc1">搞笑视频</a>
+                <a href="http://www.17sucai.com/" class="tagc2">周末娱乐</a>
+                <a href="http://www.17sucai.com/" class="tagc5">银行贷款利率</a>
+                <a href="http://www.17sucai.com/" class="tagc2">测试<a>
+                        <a href="http://www.17sucai.com/" class="tagc2">不错</a>
+                        <a href="http://www.17sucai.com/" class="tagc1">热门</a>
+                        <a href="http://www.17sucai.com/" class="tagc2">今日话题</a>
+                        <a href="http://www.17sucai.com/" class="tagc5">银行贷款利率</a>
+                        <a href="http://www.17sucai.com/" class="tagc2">银行利率</a>
+                        <a href="http://www.17sucai.com/" class="tagc2">本周话题</a>
+                        <a href="http://www.17sucai.com/" class="tagc5">非常不错</a>
+                        <a href="http://www.17sucai.com/" class="tagc2">秀春刀</a>
+                        <a href="http://www.17sucai.com/" class="tagc1">小时代</a>
+                        <a href="http://www.17sucai.com/" class="tagc2">后会无期</a>
+                        <a href="http://www.17sucai.com/" class="tagc5">银行贷款利率</a>
+                        <a href="http://www.17sucai.com/" class="tagc2">银行存款利率<a>
+                                <a href="http://www.17sucai.com/" class="tagc2">银行利率</a>
+                                <a href="http://www.17sucai.com/" class="tagc1">不孕不育</a>
+                                <a href="http://www.17sucai.com/" class="tagc2">银行存款利率</a>
+                                <a href="http://www.17sucai.com/" class="tagc5">银行贷款利率</a>
+                                <a href="http://www.17sucai.com/" class="tagc2">银行利率</a>
+                                <a href="http://www.17sucai.com/" class="tagc2">房贷利率2013</a>
+                                <a href="http://www.17sucai.com/" class="tagc5">银行存款利率表</a>
+                                <a href="http://www.17sucai.com/" class="tagc2">银行贷款利率表</a>
+
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+
+<div id="loginModal" class="modal hide fade" data-keyboard="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3>登录</h3>
+    </div>
+    <div class="modal-body">
+        <form class="form-horizontal">
+            <div class="control-group">
+                <label class="control-label">邮箱</label>
+
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-envelope"></i></span>
+                        <input type="text" name="email">
+                    </div>
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label">用户名</label>
+
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-user"></i></span>
+                        <input type="text" name="username">
+                    </div>
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label">密码</label>
+
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-lock"></i></span>
+                        <input type="password" name="password">
+                    </div>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <label class="checkbox">
+                        <input type="checkbox" checked="checked" name="remember_pwd">下次自动登录&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a>忘记密码</a>
+                    </label>
+                    <button type="submit" class="btn btn-warning" style="width:220px;*width:215px;">登录</button>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <span style="font-size:12px;color:#666;">使用其他账户登录</span> 微博、QQ
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="modal-footer">
+    </div>
+</div>
+
+<div id="registerModal" class="modal hide" data-keyboard="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3>注册</h3>
+    </div>
+    <div class="modal-body">
+        <form class="form-horizontal">
+            <div class="control-group">
+                <label class="control-label">邮箱</label>
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-envelope"></i></span>
+                        <input type="text" name="email">
+                    </div>
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label">用户名</label>
+
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-user"></i></span>
+                        <input type="text" name="username">
+                    </div>
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label">密码</label>
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-lock"></i></span>
+                        <input type="password" name="password">
+                    </div>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <button type="submit" class="btn btn-warning" style="width:220px;*width:215px;">注册</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="modal-footer">
+    </div>
+</div>
+
+
+
+
+<div id="publishModal" class="modal hide"  tabindex="-1" aria-hidden="true" role="dialog" data-keyboard="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3>注册</h3>
+    </div>
+    <div class="modal-body">
+        <form class="form-horizontal">
+            <div class="control-group">
+                <label class="control-label">标题</label>
+                <div class="controls">
+                    <input type="text" name="title" class="input-xlarge">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">链接</label>
+                <div class="controls">
+                    <input type="password" name="link" class="input-xlarge">
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <button type="submit" class="btn btn-warning" style="width:220px;*width:215px;">发布</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="modal-footer">
+    </div>
+</div>
+@include('includes.foot')
+{{HTML::script('js/index.js')}}
